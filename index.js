73b877e724e33946858ce28c53191e59ab514b58
rimiJI <hlji619@naver.com>
2025-12-15 17:30:39 +0900

    index작성

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from "express"; //express 라이브러리 임포트
+
+const app = express(); //express앱의 인스턴스 생성. calls,requests 신호 받을수있게
+const port = 3000;
+
+//The first endpoint
+//최소 하나의 엔드포인트 필요
